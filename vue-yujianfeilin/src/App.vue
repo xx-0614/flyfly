b3e6edc494da230eaee0355de86f14d3ba03ab99
xx-0614 <616100236@qq.com>
2019-07-12 11:04:30 +0800

    add .gitattributes

--- vue-yujianfeilin/src/App.vue
+++ vue-yujianfeilin/src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <!-- 引入头部组件-->
     <router-view/>
   </div>
 </template>
