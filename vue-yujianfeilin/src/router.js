62fc952c4b9a0a77ff2a1565b27b6b976150e4a8
konghongtao <konghongtao@163.com>
2019-07-12 19:45:33 +0800

    koukou

--- vue-yujianfeilin/src/router.js
+++ vue-yujianfeilin/src/router.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 // import Home from './views/Home.vue'
+import Three from  './views/Three.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {path:'/',}
     // {
     //   path: '/',
     //   name: 'home',
