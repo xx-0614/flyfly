0e4216d558cb2dccc7fe56b9f238e9ab97c7dfc5
xx-0614 <616100236@qq.com>
2019-07-13 17:46:52 +0800

    2

--- vue-yujianfeilin/src/router.js
+++ vue-yujianfeilin/src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import index from './views/index'
+import index from './views/index/index'
 import Three from  './views/Three'
 import Threetabbar from './views/Threetabbar'
 import Threetab from './views/Threetab'
---
+++ vue-yujianfeilin/src/views/index/index.vue
@@ -0,0 +1,50 @@
+<template>
+    <el-carousel :interval="3000" height="750px">
+        <el-carousel-item v-for="(item) in 3" :key="item">
+        </el-carousel-item>
+    </el-carousel>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            city:[],
+            activity:[],
+            microfilm:[],
+            travel:[],
+            love_times:[],
+            comment:[]
+        }
+    },
+    created(){
+        this.axios.get("index").then(result=>{
+            this.city=result.data.city;
+            this.activity=result.data.activity;
+            this.microfilm=result.data.microfilm;
+            this.travel=result.data.travel;
+            this.love_times=result.data.love_times;
+            this.comment=result.data.comment;
+        })
+    },
+}
+</script>
+<style>
+.el-carousel__item h3 {
+    color: #475669;
+    font-size: 18px;
+    opacity: 0.75;
+    line-height: 300px;
+    margin: 0;
+  }
+  .el-carousel__item{
+    background:url(../../assets/img/index/banner1.jpg);
+  }
+  
+  div.el-carousel__item:nth-child(4){
+    background:url(../../assets/img/index/271736095684.jpg);
+  }
+  div.el-carousel__item:nth-child(5){
+    background:url(../../assets/img/index/091400381481.jpg);
+  }
+
+</style>
